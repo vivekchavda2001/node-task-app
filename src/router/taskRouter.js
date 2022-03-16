3c3cafd04f64b18b84297cff8d38efb80c6a6fb6
vivekchavda1002 <vivek.c@simformsolutions.com>
2022-03-16 10:58:39 +0530

    TotalTask  Route Added

--- src/router/taskRouter.js
+++ src/router/taskRouter.js
@@ -2,91 +2,102 @@ const express = require('express')
 const router = express.Router()
 const Task = require('../model/tasks')
 const auth = require('../middleware/auth')
-//route for addind task along with user id
-router.post('/task',auth,async(req,res)=>{
+    //route for addind task along with user id
+router.post('/task', auth, async(req, res) => {
     const newTask = new Task({
         ...req.body,
-        owner:req.user._id
+        owner: req.user._id
     })
-    try{
+    try {
         await newTask.save()
         res.status(200).send(newTask);
-    }catch(e){
+    } catch (e) {
         res.status(400).send(e)
     }
 })
 
 //route for getting task for logged in user
-router.get('/task',auth,async (req,res)=>{
+router.get('/task', auth, async(req, res) => {
     const match = {}
     const sort = {}
-    if(req.query.completed){
+    if (req.query.completed) {
         match.completed = req.query.completed === true
     }
-    if(req.query.sortBy){
+    if (req.query.sortBy) {
         const parts = req.query.sortBy.split(':')
-        sort[parts[0]]=parts[1]==='desc'?-1:1 
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
     }
-    try{
-       
+    try {
+
         await req.user.populate({
-            path:'tasks',
+            path: 'tasks',
             match,
-            options:{
-                limit:parseInt(req.query.limit),
-                skip:parseInt(req.query.skip)
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip)
             },
             sort
         }).execPopulate()
         res.status(200).send(req.user.tasks)
 
-    }catch(e){
+    } catch (e) {
         res.status(500).send(e)
     }
-   
+
 })
-//getting specific task
-router.get('/task/:id',auth,async (req,res)=>{
-    try{
-        const task = await Task.find({_id:req.params.id,owner:req.user._id})
-            if(!task){
+
+//find total task of specific users
+router.get('/totalTask', async(req, res) => {
+
+        try {
+            const totaltask = await Task.aggregate([{ $group: { _id: "$owner", totalTask: { $sum: 1 } } }])
+            res.status(200).send(totaltask)
+        } catch (e) {
+            res.status(500).send()
+        }
+    })
+    //getting specific task
+router.get('/task/:id', auth, async(req, res) => {
+        try {
+            const task = await Task.find({ _id: req.params.id, owner: req.user._id })
+            if (!task) {
                 return res.status(404).send()
             }
             res.status(200).send(task)
-    }catch(e){
-        res.status(404).send()
-    }
-})
-//updating task
-router.patch('/task/:id',auth,async(req,res)=>{
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['description', 'completed']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+        } catch (e) {
+            res.status(404).send()
+        }
+    })
+    //updating task
+router.patch('/task/:id', auth, async(req, res) => {
+        const updates = Object.keys(req.body)
+        const allowedUpdates = ['description', 'completed']
+        const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
 
-    if (!isValidOperation) {
-        return res.status(400).send({ error:req.t('invalid_updates') })
-    }
-    try{        
-        const taskOne = await Task.findOne({_id:req.params.id,owner:req.user.id})
-        if(!taskOne){
+        if (!isValidOperation) {
+            return res.status(400).send({ error: req.t('invalid_updates') })
+        }
+        try {
+            const taskOne = await Task.findOne({ _id: req.params.id, owner: req.user.id })
+            if (!taskOne) {
+                return res.status(404).send()
+            }
+            updates.forEach((update) => taskOne[update] = req.body[update])
+            await taskOne.save()
+            res.status(200).send(taskOne)
+        } catch (e) {
+            res.status(500).send()
+        }
+    })
+    //deleting task
+router.delete('/task/:id', auth, async(req, res) => {
+    try {
+        const taskOne = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user.id })
+        if (!taskOne) {
             return res.status(404).send()
         }
-        updates.forEach((update) => taskOne[update] = req.body[update])
-        await taskOne.save()
         res.status(200).send(taskOne)
-    }catch(e){
-        res.status(500).send()
-    }
-})
-//deleting task
-router.delete('/task/:id',auth,async(req,res)=>{
-    try{
-        const taskOne = await Task.findOneAndDelete({_id:req.params.id,owner:req.user.id})
-        if(!taskOne){
-            return res.status(404).send()
-        }
-        res.status(200).send(taskOne)
-    }catch(e){
+    } catch (e) {
         res.status(500).send()
     }
 })
--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -4,119 +4,119 @@ const user = require('../model/users')
 const auth = require('../middleware/auth')
 const sharp = require('sharp')
 const multer = require('multer')
-const {sendWelcomeMail,sendCancelMail} = require('../email/account')
+const { sendWelcomeMail, sendCancelMail } = require('../email/account')
 
 //using multer for uploading files
 const upload = multer({
-    limits:{
-        fileSize:1000000
-    },
-    fileFilter(req,file,cb){
-        if(!file.originalname.match(/\.(jpg|jpeg)$/)){
-            return cb(new Error(req.t('invalid_upload')))
+        limits: {
+            fileSize: 1000000
+        },
+        fileFilter(req, file, cb) {
+            if (!file.originalname.match(/\.(jpg|jpeg)$/)) {
+                return cb(new Error(req.t('invalid_upload')))
+            }
+            cb(undefined, true)
         }
-        cb(undefined,true)
-    }
-})
-//creating new user
-router.post('/user',async(req,res)=>{
-    const newUser = new user(req.body)
-    try{
-        await newUser.save()
-        sendWelcomeMail(newUser.email,newUser.name)
-        const token = await newUser.generateAuthToken();
-        res.status(201).send({newUser,token,message: req.t('user_create_success')});
-    }catch(e){
-        res.status(400).send(e)
-    }
-    
-})
-//creating new avatar, for user
-router.post("/user/me/avatar",auth,upload.single('avatar'),async (req,res)=>{
-    const buffer = await sharp(req.file.buffer).resize({width:250,height:250}).png().toBuffer()
+    })
+    //creating new user
+router.post('/user', async(req, res) => {
+        const newUser = new user(req.body)
+        try {
+            await newUser.save()
+            sendWelcomeMail(newUser.email, newUser.name)
+            const token = await newUser.generateAuthToken();
+            res.status(201).send({ newUser, token, message: req.t('user_create_success') });
+        } catch (e) {
+            res.status(400).send(e)
+        }
+
+    })
+    //creating new avatar, for user
+router.post("/user/me/avatar", auth, upload.single('avatar'), async(req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
     req.user.avatar = buffer
     await req.user.save()
     res.status(200).send()
-},(error,req,res,next)=>{
-    res.status(400).send({error:error.message})
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
 })
 
 //deleting avatar for user
-router.delete("/user/me/avatar",auth,upload.single('avatar'),async (req,res)=>{
+router.delete("/user/me/avatar", auth, upload.single('avatar'), async(req, res) => {
     req.user.avatar = undefined
     await req.user.save()
     res.status(200).send()
-},(error,req,res,next)=>{
-    res.status(400).send({error:error.message})
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
 })
 
 //getting details of logged in user
-router.get('/user/me',auth,async(req,res)=>{
-   res.send(req.user)   
-})
-//
-router.get('/user',auth,async(req,res)=>{
+router.get('/user/me', auth, async(req, res) => {
+        res.send(req.user)
+    })
+    //
+router.get('/user', auth, async(req, res) => {
 
-    try{
-        const users = await user.find({})
-         res.status(200).send(users)
-    }catch(e){
-        res.status(500).send()
-    }
-})
-//fetching profile pic of user
-router.get('/user/pic/:id',async (req,res)=>{
-    try{       
-        const userOne = await user.findById(req.params.id)
-        if(!userOne || !userOne.avatar){
-            throw new Error("")
+        try {
+            const users = await user.find({})
+            res.status(200).send(users)
+        } catch (e) {
+            res.status(500).send()
         }
-        res.set('Content-Type','image/png')
-        res.send(userOne.avatar)
-    }catch(e){
-        res.status(500).send(e)
-    }
-})
-//login route for user
-router.post('/user/login', async (req, res) => {
+    })
+    //fetching profile pic of user
+router.get('/user/pic/:id', async(req, res) => {
+        try {
+            const userOne = await user.findById(req.params.id)
+            if (!userOne || !userOne.avatar) {
+                throw new Error("")
+            }
+            res.set('Content-Type', 'image/png')
+            res.send(userOne.avatar)
+        } catch (e) {
+            res.status(500).send(e)
+        }
+    })
+    //login route for user
+router.post('/user/login', async(req, res) => {
+        try {
+            const userOne = await user.findByCredentials(req.body.email, req.body.password)
+            const token = await userOne.generateAuthToken()
+            res.status(200).send({ userOne, token })
+        } catch (e) {
+            res.status(400).send({ msg: "Invalid Credentials" })
+        }
+    })
+    //logout user
+router.post('/user/logout', auth, async(req, res) => {
+        try {
+            req.user.tokens = req.user.tokens.filter((token) => {
+                return token.token !== req.token
+            })
+            await req.user.save()
+            console.log(req.token);
+            res.status(200).send(req.t('logged_out'))
+        } catch (e) {
+            res.status(500).send()
+        }
+    })
+    //logout from all the devices 
+router.post('/user/logoutall', auth, async(req, res) => {
     try {
-        const userOne = await user.findByCredentials(req.body.email, req.body.password)
-        const token = await userOne.generateAuthToken()
-        res.status(200).send({userOne,token})
-    } catch (e) {
-        res.status(400).send({msg:"Invalid Credentials"})
-    }
-})
-//logout user
-router.post('/user/logout',auth,async (req,res)=>{
-    try{
-        req.user.tokens = req.user.tokens.filter((token)=>{
-            return token.token !== req.token
-        })
-        await req.user.save()
-        console.log(req.token);
-        res.status(200).send(req.t('logged_out'))
-    }catch(e){
-        res.status(500).send()
-    }
-})
-//logout from all the devices 
-router.post('/user/logoutall',auth,async (req,res)=>{
-    try{
-        req.user.tokens = req.user.tokens.filter((token)=>{
+        req.user.tokens = req.user.tokens.filter((token) => {
             return token.token === ""
         })
         await req.user.save()
         console.log(req.token);
         res.status(200).send('Logged Out!!')
-    }catch(e){
+    } catch (e) {
         res.status(500).send()
     }
 })
 
 //updating user details
-router.patch('/user/me',auth, async (req, res) => {
-    const updates = Object.keys(req.body)    
+router.patch('/user/me', auth, async(req, res) => {
+    const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
     if (!isValidOperation) {
@@ -132,13 +132,13 @@ router.patch('/user/me',auth, async (req, res) => {
 })
 
 //deleting profile of user
-router.delete('/user/me',auth,async(req,res)=>{
-    try{
-        await req.user.remove() 
-        sendCancelMail(req.user.email,req.user.name)
-        res.status(201).send({message:req.t('profile_deleted')})
-    }catch(e){
-        res.status(500).send({message:req.t('profile_not_exist')})
+router.delete('/user/me', auth, async(req, res) => {
+    try {
+        await req.user.remove()
+        sendCancelMail(req.user.email, req.user.name)
+        res.status(201).send({ message: req.t('profile_deleted') })
+    } catch (e) {
+        res.status(500).send({ message: req.t('profile_not_exist') })
     }
 })
 module.exports = router;
